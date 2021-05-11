40dac14188d8b5ec1821a1b7fc35cb7978061a0c
Jeffrey Wang <j@mywikis.com>
2021-05-11 02:17:36 -0500

    add formatter tests

---
+++ tests/FormattersTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+require_once(__DIR__ . '/../includes/formatters.php');
+
+use PHPUnit\Framework\TestCase;
+
+final class TestValidators extends TestCase {
+    public function testValidateLastName() {
+        $this->assertTrue(Validators::last_name('Doe'));
+    }
+
+    public function testCurrencyConverter() {
+        $this->assertStringStartsWith(
+            'In USD: $',
+            Formatters::currency_converter('3.22', 'EUR');
+        );
+    }
+
+    public function testDonationAmount() {
+        $this->assertEquals(
+            '€4.20',
+            Formatters::donation_amount('4.20', 'EUR');
+        );
+    }
+}
