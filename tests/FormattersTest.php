0e74fbc80d9ffc6f36344f0f8651b5ca4e72cae1
Jeffrey Wang <j@mywikis.com>
2021-05-11 02:32:10 -0500

    autoload

--- tests/FormattersTest.php
+++ tests/FormattersTest.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require(__DIR__ . '/../vendor/autoload.php');
+
 require_once(__DIR__ . '/../includes/formatters.php');
 
 use PHPUnit\Framework\TestCase;
--- tests/ValidatorsTest.php
+++ tests/ValidatorsTest.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require(__DIR__ . '/../vendor/autoload.php');
+
 require_once(__DIR__ . '/../includes/validators.php');
 
 use PHPUnit\Framework\TestCase;
