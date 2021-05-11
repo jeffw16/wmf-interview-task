59c4b56cd0015d04b26b11a8600da374e610c415
Jeffrey Wang <j@mywikis.com>
2021-05-11 03:17:22 -0500

    frogot ; wdiofgjwoifew

--- tests/DonationSubmissionTest.php
+++ tests/DonationSubmissionTest.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../settings.php')
+require(__DIR__ . '/../settings.php');
 require_once(__DIR__ . '/../includes/DonationSubmission.php');
 
 use PHPUnit\Framework\TestCase;
